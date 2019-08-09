723b6d153777e0495d072c90b37c0649deca34fa
ShriLingam23 <trnshri23@gmail.com>
2019-08-09 15:59:52 +0530

    Switch condition complexity measure implementation

--- Back_End/Complexity_Algorithm/ComplexityByType.js
+++ Back_End/Complexity_Algorithm/ComplexityByType.js
@@ -7,7 +7,7 @@ var complexityByType = function(lineArr){
         var count =0;
         if(lineArr[i].includes("if")) {
             count++;
-            console.log(count)
+            // console.log(count)
             if(lineArr[i].includes("&") || lineArr[i].includes("|")) {
                 var ch = lineArr[i].split('');
                 for(x=0 ; x<ch.length;x++) {
@@ -20,11 +20,11 @@ var complexityByType = function(lineArr){
                         }
                     }
                 }
-                console.log(count)
+                // console.log(count)
             }
         } else if(lineArr[i].includes("for") || lineArr[i].includes("while")|| lineArr[i].includes("do") ) {
             count+=2;
-            console.log(count)
+            // console.log(count)
             if(lineArr[i].includes("&") || lineArr[i].includes("|")) {
                 var ch = lineArr[i].split('');
                 for(x=0 ; x<ch.length ; x++) {
@@ -37,13 +37,48 @@ var complexityByType = function(lineArr){
                         }
                     }
                 }
-                console.log(count)
+                // console.log(count)
             }
         } else if(lineArr[i].includes("catch")) {
             count++;
-        } else if(lineArr[i].includes("switch")) {
+
+        } else if(lineArr[i].includes("switch") && lineArr[i].includes("(") && lineArr[i].includes(")")) {
             
-            
+            var caseCount = 0
+
+            if(lineArr[i].includes("case"))
+                caseCount++;
+
+            var checkArr =[]
+
+            //Loop till end of programme assuming switch continue
+            for(s=i;s<lineArr.length;s++){
+
+                var charArr = lineArr[s].split('');
+
+                //looping through each character
+                for(c=0;c<charArr.length;c++){
+
+                    if(charArr[c]=='{')
+                        checkArr.push('{')
+                    else if(charArr[c]=='}'){
+                        if(checkArr.length!=0){
+                            checkArr.pop()
+
+                            if(checkArr.length==0)
+                                break;
+                        }
+                            
+                    }
+                }
+
+                if(checkArr.length==0)
+                    break;
+                    
+
+            }
+
+            count+=caseCount;
             
         }
 
